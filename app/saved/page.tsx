5d8e7fb4ec0630b8dfbf59f288e8036d119512e4
Hein Htet Zaw <105983739+Hendrickhz@users.noreply.github.com>
2024-06-19 17:55:40 +0630

    session user condition fixed

--- app/saved/page.tsx
+++ app/saved/page.tsx
@@ -1,110 +1,16 @@
-"use client";
-import NoRecipesAvailable from "@/components/NoRecipesAvailable";
-import RecipeCard from "@/components/RecipeCard";
-import { Recipe } from "@/interfaces";
-import {
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogOverlay,
-  Box,
-  Button,
-  Divider,
-  Flex,
-  Heading,
-  IconButton,
-  Image,
-  Link,
-  Spinner,
-  Text,
-  useDisclosure,
-} from "@chakra-ui/react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import React, { useEffect, useRef, useState } from "react";
-import { FaEdit, FaTrash } from "react-icons/fa";
-import { toast } from "react-toastify";
+import SavedRecipesSection from "@/components/SavedRecipesSection";
+import { Box, Heading } from "@chakra-ui/react";
 
 const SavedPage = () => {
-  const { data: session, status } = useSession();
-  const [recipes, setRecipes] = useState([]);
-
-  //loading management
-  const [isLoading, setIsLoading] = useState(true);
-  const loading = status == "loading";
-  const pageLoading = loading && isLoading;
- 
-  useEffect(() => {
-    const fetchProfileRecipes = async () => {
-      if (!session) return;
-      try {
-        const res = await fetch(`/api/saved/${session.user.id}`);
-        if (res.status == 200) {
-          const data = await res.json();
-          setRecipes(data);
-        } else {
-          toast.error("Failed to fetch recipes.");
-        }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchProfileRecipes();
-  }, [session]);
-  if (pageLoading) {
-    return (
-      <Box
-        as="section"
-        className="  h-[70vh] w-screen flex justify-center  items-center"
-      >
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="orange.500"
-          size="xl"
-        />
-      </Box>
-    );
-  }
   return (
     <Box as="section" className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 py-6">
-     
       {/* recipes section  */}
       <Heading as={"h2"} fontFamily={"serif"}>
         Saved Recipes
       </Heading>
-      <>
-        {recipes.length > 0 ? (
-          <Box className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  my-4  gap-6">
-            {" "}
-            {recipes.map((recipe: Recipe) => (
-              <RecipeCard
-                key={recipe._id}
-                recipe={recipe}
-              />
-            ))}
-          </Box>
-        ) : (
-          <>
-            <NoRecipesAvailable isSavedPage={true} />
-          </>
-        )}
-      </>
-      {/* recipes section  */}
+      <SavedRecipesSection />
     </Box>
   );
 };
 
 export default SavedPage;
-
-interface IRecipeProps {
-  _id: string;
-  title: string;
-  thumbnailUrl: string;
-}
-
---
+++ components/SavedRecipesSection.tsx
@@ -0,0 +1,81 @@
+"use client";
+import NoRecipesAvailable from "@/components/NoRecipesAvailable";
+import RecipeCard from "@/components/RecipeCard";
+import { Recipe } from "@/interfaces";
+import { Box, Spinner } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+export interface User {
+  id: string; // or number, depending on your ID type
+  name?: string; // other user properties
+  email?: string; // other user properties
+}
+
+export interface CustomSession {
+  user: User;
+}
+const SavedRecipesSection = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const [recipes, setRecipes] = useState([]);
+
+  //loading management
+  const [isLoading, setIsLoading] = useState(true);
+  const loading = status == "loading";
+  const pageLoading = loading && isLoading;
+  useEffect(() => {
+    const fetchProfileRecipes = async () => {
+      if (!session || !session.user) return;
+      try {
+        const res = await fetch(`/api/saved/${session.user.id}`);
+        if (res.status == 200) {
+          const data = await res.json();
+          setRecipes(data);
+        } else {
+          toast.error("Failed to fetch recipes.");
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchProfileRecipes();
+  }, [session]);
+  if (pageLoading) {
+    return (
+      <Box
+        as="section"
+        className="  h-[70vh] w-screen flex justify-center  items-center"
+      >
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="orange.500"
+          size="xl"
+        />
+      </Box>
+    );
+  }
+  return (
+    <>
+      {recipes.length > 0 ? (
+        <Box className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  my-4  gap-6">
+          {" "}
+          {recipes.map((recipe: Recipe) => (
+            <RecipeCard key={recipe._id} recipe={recipe} />
+          ))}
+        </Box>
+      ) : (
+        <>
+          <NoRecipesAvailable isSavedPage={true} />
+        </>
+      )}
+    </>
+  );
+};
+
+export default SavedRecipesSection;
